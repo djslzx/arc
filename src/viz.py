9b0a20eb269c5fe5698dc537599b77202cca73de
David J. Lee <djslzx@gmail.com>
2021-11-24 22:54:13 -0500

    clean up viz api, add seaborn to viz

--- src/viz.py
+++ src/viz.py
@@ -1,3 +1,6 @@
+import sys
+import json
+import seaborn as sns
 import torch as T
 import numpy as np
 import matplotlib.pyplot as plt
@@ -6,9 +9,12 @@ import matplotlib.patheffects as pe
 WIDTH=10
 HEIGHT=10
 
+sns.set_theme(style='white')
+
 def clean(fname):
     name = '.'.join(fname.split('.')[:-1])
-    return '/'.join(name.split('/')[1:])
+    name = name.split('/')[-1]
+    return name
 
 def label(ax, grid):
     for (y,x), text in np.ndenumerate(grid):
@@ -18,43 +24,45 @@ def label(ax, grid):
                     fontsize='x-small',
                     path_effects=[pe.withStroke(linewidth=2, foreground="black")])
 
-def output(fname, show=True, save=False):
+def output(fname=None, show=True):
     if show: 
         plt.show()
-    if save and fname is not None:
+    if fname is not None:
         print(f'Saving figure to {fname}...')
         plt.gcf().set_size_inches(WIDTH, HEIGHT)
         plt.savefig(fname)
     plt.close()
 
 def viz_task(name, i, x, y):
-    vmin=min(x.min().item(), y.min().item())
-    vmax=max(x.max().item(), y.max().item())    
+    vmin=0 #min(x.min().item(), y.min().item())
+    vmax=9 #max(x.max().item(), y.max().item())    
+    x_masked = np.ma.masked_where(x == 0, x)
+    y_masked = np.ma.masked_where(y == 0, y)
 
     f, ax = plt.subplots(2,2) 
     f.suptitle(f'{name}-{i}')
     ax[0, 0].set_title('in')
-    ax[0, 0].imshow(x, vmin=vmin, vmax=vmax)
+    ax[0, 0].imshow(x_masked, vmin=vmin, vmax=vmax)
     ax[0, 1].set_title('out')
-    ax[0, 1].imshow(y, vmin=vmin, vmax=vmax)
+    ax[0, 1].imshow(y_masked, vmin=vmin, vmax=vmax)
 
-    ax[1, 0].imshow(x, vmin=vmin, vmax=vmax)
-    ax[1, 1].imshow(y, vmin=vmin, vmax=vmax)
+    ax[1, 0].imshow(x_masked, vmin=vmin, vmax=vmax)
+    ax[1, 1].imshow(y_masked, vmin=vmin, vmax=vmax)
     label(ax[1, 0], x)
     label(ax[1, 1], y)
 
-    output(f'images/{name}-{i}.png', save=True, show=False)
+    output()
+    # output(f'../tasks/images/{name}-{i}.png', show=False)
 
 def viz_json(fname):
     data = json.load(open(fname, 'r'))
     name = clean(fname)
-    print(name)
     for i, d in enumerate(data['train']):
         x, y = d['input'], d['output']
         x, y = T.tensor(x), T.tensor(y)
         viz_task(name, i, x, y)
 
-def viz(t, title='', subtitle='', fname=None, show=True, save=False):
+def viz(t, title='', subtitle='', fname=None, show=True):
     f, ax = plt.subplots(2, 1)
     f.suptitle(title)
     plt.figtext(0.5, 0.01, subtitle, wrap=True, horizontalalignment='center', fontsize=9)
@@ -62,13 +70,13 @@ def viz(t, title='', subtitle='', fname=None, show=True, save=False):
     ax[1].imshow(t)
     label(ax[1], t)
 
-    output(fname, show, save)
+    output(fname, show)
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print("Usage: viz.py file.json")
     else:
         fname = sys.argv[1]
-
+        viz_json(fname)
 
 
