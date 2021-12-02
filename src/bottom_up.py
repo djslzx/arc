b5c49a3c8622178ed0624d008aee30bacd7653ba
David J. Lee <djslzx@gmail.com>
2021-12-02 16:57:51 -0500

    update bottom-up to work with grammar

--- src/bottom_up.py
+++ src/bottom_up.py
@@ -117,7 +117,7 @@ def integer_partitions(target_value, number_of_arguments):
 
 def test_bottom_up():
     grammar = Grammar(
-        ops=[Stack, Rect, Line, Point, Plus, Minus, Times, If, And, Not],
+        ops=[Join, Rect, Line, Point, Plus, Minus, Times, If, And, Not],
         consts=[Num(i) for i in range(Z_SIZE)] + [Z(i) for i in range(Z_SIZE)],
     )
 
@@ -138,12 +138,12 @@ def test_bottom_up():
                Num(4), Num(4)).eval({}))],
         # U((R Z1 Z1 Z2 Z2) (R Z2 Z2 Z3 Z3))
         [({"z": [0, 1, 2, 3, 4, 4]},
-          Stack(Rect(Num(1), Num(1),
+          Join(Rect(Num(1), Num(1),
                      Num(2), Num(2)),
                 Rect(Num(2), Num(2), 
                      Num(3), Num(3))).eval({})),
          ({"z": [1, 2, 3, 4, 4, 4]},
-          Stack(Rect(Num(2), Num(2),
+          Join(Rect(Num(2), Num(2),
                      Num(3), Num(3)),
                 Rect(Num(3), Num(3), 
                      Num(4), Num(4))).eval({}))
@@ -171,7 +171,7 @@ def test_bottom_up():
 
 def test_bottom_up_tensor():
     grammar = Grammar(
-        ops=[Stack, Rect, Plus, Minus, Times],
+        ops=[Join, Rect, Plus, Minus, Times, Apply, HFlip, VFlip, Translate],
         consts= [Z(i) for i in range(Z_SIZE)] # [Num(i) for i in range(5)]
     )
 
