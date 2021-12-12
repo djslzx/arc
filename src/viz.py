e5f9090aebbe3827f11a7c734a68bf644a456f51
David J. Lee <djslzx@gmail.com>
2021-12-11 23:21:26 -0500

    refactor viz

--- src/viz.py
+++ src/viz.py
@@ -6,7 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patheffects as pe
 
-WIDTH=10
+WIDTH=8
 HEIGHT=10
 
 sns.set_theme(style='white')
@@ -24,67 +24,67 @@ def label_cells(ax, grid):
                     fontsize='x-small',
                     path_effects=[pe.withStroke(linewidth=2, foreground="black")])
 
+def show(ax, t, label=False):
+    t = T.tensor(t) if not isinstance(t, T.Tensor) else t
+    t = np.ma.masked_where(t == 0, t)
+    ax.axes.xaxis.set_visible(False)
+    ax.axes.yaxis.set_visible(False)
+    ax.imshow(t, vmin=0, vmax=9)
+    if label:
+        label_cells(ax, t)
+
 def output(fname=None, show=True):
     if show: 
         plt.show()
     if fname is not None:
         print(f'Saving figure to {fname}...')
         plt.gcf().set_size_inches(WIDTH, HEIGHT)
-        plt.savefig(fname)
+        plt.subplots_adjust(wspace=-0.55, hspace=0.1)
+        # plt.gcf().tight_layout()
+        plt.savefig(fname, bbox_inches='tight')
     plt.close()
 
-def viz_task(name, i, x, y):
-    vmin=0 #min(x.min().item(), y.min().item())
-    vmax=9 #max(x.max().item(), y.max().item())    
-    x_masked = np.ma.masked_where(x == 0, x)
-    y_masked = np.ma.masked_where(y == 0, y)
-
-    f, ax = plt.subplots(2,2) 
-    f.suptitle(f'{name}-{i}')
-    ax[0, 0].set_title('in')
-    ax[0, 0].imshow(x_masked, vmin=vmin, vmax=vmax)
-    ax[0, 1].set_title('out')
-    ax[0, 1].imshow(y_masked, vmin=vmin, vmax=vmax)
-
-    ax[1, 0].imshow(x_masked, vmin=vmin, vmax=vmax)
-    ax[1, 1].imshow(y_masked, vmin=vmin, vmax=vmax)
-    label_cells(ax[1, 0], x)
-    label_cells(ax[1, 1], y)
-
-    output()
-    # output(f'../tasks/images/{name}-{i}.png', show=False)
-
-def viz_json(fname):
+def viz_json(fname, save=False):
     data = json.load(open(fname, 'r'))
     name = clean(fname)
-    for i, d in enumerate(data['train']):
-        x, y = d['input'], d['output']
-        x, y = T.tensor(x), T.tensor(y)
-        viz_task(name, i, x, y)
+    xs = [d['input'] for d in data['train']]
+    ys = [d['output'] for d in data['train']]
+    viz_sample(xs, ys, title=f'pdfs/{name}.pdf')
 
-def viz(t, title='', subtitle='', fname=None, show=True):
-    f, ax = plt.subplots(2, 1)
-    f.suptitle(title)
+def viz_sample(xs, ys, title='', txt='', save=False):
+    '''Visualize an nx2 grid of input/output pairs'''
+    f, ax = plt.subplots(len(xs), 2)
+    # f.suptitle(title)
+    plt.figtext(0.5, 0.01, txt, wrap=True, horizontalalignment='center', fontsize=9)    
+    for i, (x, y) in enumerate(zip(xs, ys)):
+        show(ax[i, 0], x)
+        show(ax[i, 1], y)
+    # output()
+    output(title, show=False)
+
+def viz_mult(ts, text=''):
+    f, ax = plt.subplots(len(ts), 1)
+    plt.figtext(0.5, 0.01, text, wrap=True, horizontalalignment='center', fontsize=9)
+    for i, t in enumerate(ts):
+        show(ax[i], t)
+    output(show=True)
+
+def viz(t, title='', subtitle='', fname=None, label=False):
+    # f, ax = plt.plot() # subplots(2, 1)
+    f, ax = plt.gcf(), plt.gca()
+    plt.title(title)
     plt.figtext(0.5, 0.01, subtitle, wrap=True, horizontalalignment='center', fontsize=9)
-    t_masked = np.ma.masked_where(t == 0, t)
-    ax[0].imshow(t_masked, vmin=0, vmax=9)
-    ax[1].imshow(t_masked, vmin=0, vmax=9)
-    # ax[0].grid()
-    # ax[1].grid()
-    label_cells(ax[1], t_masked)
-    output(fname, show)
+    show(ax, t, label=label)
+    output(fname, show=True)
 
-def viz_sample(samples, n, txt=''):
+def viz_grid(samples, n, txt=''):
     '''Visualize an nxn grid of samples'''
     f, ax = plt.subplots(n, n)
     plt.figtext(0.5, 0.01, txt, wrap=True, horizontalalignment='center', fontsize=9)    
     for i, sample in enumerate(samples):
         x, y = i%n, i//n
-        masked = np.ma.masked_where(sample == 0, sample)
-        ax[y, x].imshow(masked, vmin=0, vmax=9)
-        ax[y, x].axes.xaxis.set_visible(False)
-        ax[y, x].axes.yaxis.set_visible(False)
-        # label_cells(ax[y,x], masked)
+        show(ax[y, x], sample)
+
     output()
 
 if __name__ == '__main__':
