99bc067b67cb7886b61e375abbcf8932c535ef5c
David Jinsoo Lee <djslzx@gmail.com>
2022-02-13 10:19:42 -0500

    add zful/zless slurm scripts

--- slurm/tf.sh
+++ slurm/tf.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 eval "$(conda shell.bash hook)"
 conda activate tenv
-python3 -u /home/djl328/arc/src/transformer.py
+python3 -u /home/djl328/arc/src/transformer.py train /home/djl328/arc/data/10k-zless-exs.dat 1
