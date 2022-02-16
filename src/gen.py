3c9c84ad9cbca0628434bab7fcfb76c3adcc5fa4
David J. Lee <djslzx@gmail.com>
2022-02-16 17:11:57 -0500

    track num uniq exprs

--- src/gen.py
+++ src/gen.py
@@ -300,6 +300,15 @@ def list_exs(fname):
     for bmps, tokens in util.load_incremental(fname):
         print(deserialize(tokens), f'{len(bmps)} bitmaps')
 
+def count_uniq(fname):
+    d = {}
+    for bmps, tokens in util.load_incremental(fname):
+        toks = deserialize(tokens)
+        elts = toks.bmps
+        for e in elts:
+            d[e] = d.get(e, 0) + 1
+    return d
+
 if __name__ == '__main__':
 
     # test_rm_dead_code()
@@ -313,18 +322,24 @@ if __name__ == '__main__':
     #     print('expr:', d, len(d))
     #     print(viz_grid(bmps[:25], d))
 
-    a_gram = Grammar(ops=[Plus, Minus, Times],
-                     consts=([Z(i) for i in range(LIB_SIZE)] +
-                             [Num(i) for i in range(Z_LO, Z_HI + 1)]))
-    print(a_gram.ops, a_gram.consts)
+    # a_gram = Grammar(ops=[Plus, Minus, Times],
+    #                  consts=([Z(i) for i in range(LIB_SIZE)] +
+    #                          [Num(i) for i in range(Z_LO, Z_HI + 1)]))
+    # print(a_gram.ops, a_gram.consts)
 
-    make_exprs(n_exprs=10_000, n_envs=1, max_n_entities=2, a_bound=1,
-               entities=[# Point, Line,
-                         Rect],
-               a_grammar = a_gram,
-               cmps_loc='../data/10k-simple-cmps.dat',
-               exprs_loc='../data/10k-simple-exs.dat',
-               load_pool=False,
-               max_zs=0)
+    # make_exprs(n_exprs=10_000, n_envs=1, max_n_entities=2, a_bound=1,
+    #            entities=[# Point, Line,
+    #                      Rect],
+    #            a_grammar = a_gram,
+    #            cmps_loc='../data/10k-simple-cmps.dat',
+    #            exprs_loc='../data/10k-simple-exs.dat',
+    #            load_pool=False,
+    #            max_zs=0)
 
-    # list_exs('../data/10k-simple-exs.dat')
+    u = count_uniq('../data/10k-simple-exs.dat')
+    n = {}
+    for k, v in u.items():
+        n[v] = n.get(v, []) + [k]
+    for k, v in sorted(n.items()):
+        print(k, v)
+
