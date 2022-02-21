bc757a4c86516631e9b7322d1f707499616b9677
David Jinsoo Lee <djslzx@gmail.com>
2022-02-20 23:01:36 -0500

    update slurm scripts

--- slurm/tf.sh
+++ slurm/tf.sh
@@ -3,10 +3,13 @@ eval "$(conda shell.bash hook)"
 conda activate tenv
 python3 -u /home/djl328/arc/src/transformer.py \
 	--train \
-	--name 'simple' \
-	--data /home/djl328/arc/data/10k-simple-exs.dat \
-	-n 1 \
+	--name '10k-2r2z-2048' \
+	--training-data /home/djl328/arc/data/10000-2r2z-train.exs \
+	--validation-data /home/djl328/arc/data/10000-2r2z-test.exs \
+	--model-dim 2048 \
+	--learning-rate 0.00005 \
+	-n 5 \
 	--epochs 1000000 \
-	--batch-size 32 \
+	--batch-size 64 \
 	--sample-freq 10 \
-	--log-freq 1
+	--log-freq 3
