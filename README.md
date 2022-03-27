62a460490bdd3f61dcccc210176b1e499ed13493
David J. Lee <djslzx@gmail.com>
2022-03-27 17:27:41 -0400

    generate example programs in parallel; update README

--- README.md
+++ README.md
@@ -1,79 +1,18 @@
 # arc
 
-## Strategy
-Use inductive synthesis (bottom-up enumeration with decision trees) to find `f'` given `x_i` and `z_i` such that `f'(z_i) ~ x_i` for all `x_i`.
-
-Once an `f'` is found that approximates each `x_i` (minimizing wrt L2 norm (sum of squared diffs)), fix `f'` and perturb random inputs `z_i` giving `z_i'` so that `f'(z')` better approximates `x_i`.
-
-## Notes
-### Measuring instance complexity / bounding search depth for f
-We need some principled way of trading off between optimizing f and optimizing z, instead of treating f optimization rounds as a hyperparameter.
-
-One way of doing this might be to somehow measure the complexity in a bitmap and using this to bound the search depth.
-
-Alternatively, we can think of a way of iteratively increasing depth while optimizing z -- maybe optimize z at each depth level? Is there some way we can use dynamic programming to avoid redundancy here?
-
-
-### Better error functions
-Currently, the distance metric treats Rect(0,0,1,1) and Rect(3,3,4,4) as the same distance from Rect(1,1,2,2), although the first expression is a better approximation of the target. As another example, consider the following:
-
-Target
+## Training policy and value networks
+- Pretrain policy network using loss fn that minimizes loss over desired action sequences:
 ```
-#___
-#___
-____
-____
+  L_pt(policy) = E[sum{t <= T} log policy(action_t | state, spec)]
+               = E[sum{t <= T} log policy(d_t | f', B', B)]
 ```
-
-A
+- Train value function by sampling rollouts from the policy network and computing heuristic value functions offline:
 ```
-##__
-##__
-____
-____
+  L_train(policy, value) = R * sum{t <= T} log value(state_t, spec) 
+                           + (1 - R) * sum{t <= T} log(1 - value(state_t, spec))
+                           + R * sum{t <= T} log policy(a_t | state_t, spec)
+                         = R * sum{t <= T} log value(f', B', B) 
+                           + (1 - R) * sum{t <= T} log(1 - value(f', B', B))
+                           + R * sum{t <= T} log policy(d_t | f', B', B)
+
 ```
-
-B
-```
-____
-____
-____
-___#
-```
-
-If we only count pixel differences, B is closer to Target than A even though in the grammar, A is only one edit away from Target.
-
-It seems that using some notion of edit distance on the grammar would work better here, but this would require having access to a ground truth expression, which we don't have.
-
-### Keeping local optima across iterations
-It seems that carrying through optimal solutions as candidates in the next phase of the search (using prior Z values as candidates for future Z's, or prior f's for future f's) hampers the search from considering other solutions.
-
-### Grammar size
-It looks like using a larger grammar (i.e. w/ arithmetic expressions) leads the search away from the optimal solution for simple expressions that don't require arithmetic expressions, e.g. 
-```
-[
-  ({}, Rect(Point(Num(0), Num(0)), Point(Num(1), Num(1)))),
-  ({}, Rect(Point(Num(1), Num(1)), Point(Num(2), Num(2)))),
-],
-```
-The search space still includes the optimal solutions, because we're augmenting the grammar. So it seems that there's something about the way we conduct the search that prevents these optimal, shorter solutions from being considered over longer and less optimal solutions.  Maybe longer solutions perform better within each round, so we never hold onto a shorter solution long enough to optimize Z to fit it.
-
-Increasing the number of random samples in the `opt_z` step seems to help, but we still get overcomplicated expressions to which we fit Z:
-```
-Synthesized program:	 ((z_n[3], z_n[3]), (z_n[5], (- z_n[0] z_n[4]))), 
-Z: [([3, 1, 2, 0, 2, 1], [True, True, True, True, True, True]), ([3, 3, 3, 1, 1, 2], [True, True, False, False, False, True])] in 1.370865821838379 seconds
-```
-
-### Storing/managing `z`
-- associate one `z_i` with each `x_i` and learn a pattern for manipulating `z_i` to get `x_i`
-- generate each `z_i` as part of the problem input?
-
-### False and Num(0)
-Python treats `bool` as a subclass of `int`, so 
-```
-False == 0
->> True
-```
-
-This means that generated programs will conflate `False` and `0`. (We don't need to worry about `True` because we don't have a `True` literal.)
-
--- src/data.py
+++ src/data.py
@@ -1,57 +1,87 @@
 """
 Generate data to train value & policy nets
 """
-import math
+import pickle
 import time
-import random
-import torch as T
-import itertools as it
 import typing as typ
 import multiprocessing as mp
+import joblib
 
 from grammar import *
-from bottom_up import bottom_up_generator
 import util
 import viz
 
-def gen_data(n_envs: int, n_programs: int, n_lines: typ.Union[int, tuple[int, int]],
-             rand_arg_bounds: tuple[int, int],
-             line_types: list[type], line_type_weights: list[float] = None,
-             n_workers: int = 1):
-    """
-    Samples a set of programs of the form `Seq(l_1, l_2. ..., l_k)`.
-    """
-    uid = time.time()
-    # generate a set of `n_envs` environments
-    pass
-    
-def gen_data_worker(fname: str, n_envs: int, n_programs: int, n_lines: int,
-                    arg_exprs: list[Expr],
+
+def gen_policy_data(fname_prefix: str,
+                    n_envs: int, n_programs: int, n_lines_bounds: tuple[int, int],
                     rand_arg_bounds: tuple[int, int],
-                    line_types: list[type], line_type_weights: list[float]):
+                    line_types: list[type], line_type_weights: typ.Optional[list[float]] = None,
+                    n_programs_per_worker: int = 100):
+    """
+    Generates data for pretraining policy net by sampling a set of programs of the form
+    `Seq(l_1, l_2. ..., l_k)`, where k is in n_lines_bounds
+    """
+    n_lines_lo, n_lines_hi = n_lines_bounds
+    assert n_lines_lo >= 0, f'Expected n_lines_bounds > 1, found {n_lines_bounds}'
+    
+    fname = f'{fname_prefix}policy-{int(time.time())}'
+    arg_exprs = [Z(i) for i in range(LIB_SIZE)] + [Num(i) for i in range(0, 10)]
+    
+    # run n_workers workers to generate a total of n_programs programs of each size in n_lines
+    n_workers = n_programs // n_programs_per_worker + (n_programs % n_programs_per_worker > 0)
+    
+    with mp.Pool(processes=n_workers) as pool:
+        pool.starmap(worker_gen_policy_data,
+                     [(f'{fname}/{i}.dat', n_envs, n_programs_per_worker, n_lines,
+                       arg_exprs, rand_arg_bounds, line_types, line_type_weights, True)
+                      for n_lines in range(n_lines_lo, n_lines_hi+1)
+                      for i in range(n_workers)])
+    
+def worker_gen_policy_data(fname: str, n_envs: int, n_programs: int, n_lines: int,
+                           arg_exprs: list[Expr],
+                           rand_arg_bounds: tuple[int, int],
+                           line_types: list[type], line_type_weights: list[float],
+                           verbose: bool = False):
     """
     Generate `(f, z)` pairs with associated training outputs (value, policy).
     """
-    util.touch(fname)
-    for i in range(n_programs):
-        envs = seed_libs(n_envs)
+    util.make_parent_dir(fname)
+    with open(fname, 'wb') as file:
+        for i in range(n_programs):
+            envs = seed_libs(n_envs)
+            p = gen_program(envs=envs, arg_exprs=arg_exprs, n_lines=n_lines,
+                            rand_arg_bounds=rand_arg_bounds,
+                            line_types=line_types, line_type_weights=line_type_weights)
+            bitmaps = T.stack([p.eval(env) for env in envs])
+            p = p.simplify_indices()
+            if verbose: print(f'[{i}/{n_programs}]: {p}')
+            pickle.dump((p, envs, bitmaps), file)
 
-        # choose n_lines lines compatible with env
-        lines = gen_lines(envs=envs, arg_exprs=arg_exprs, n_lines=n_lines,
-                          rand_arg_bounds=rand_arg_bounds,
-                          line_types=line_types, line_type_weights=line_type_weights)
-        f = Seq(*lines)
-        util.save(data=(f, envs), fname=fname, append=True)
-
-def gen_lines(envs: list[dict], arg_exprs: list[Expr], n_lines: int, rand_arg_bounds: tuple[int, int],
-              line_types: list[type], line_type_weights: list[float] = None,
-              verbose=False):
+def compute_value(expr, bitmaps):
     """
-    Generates a sequence of lines.
+    Compute vector of values outputted by value function.
+    - log P(B|f')
+    - I[f' is a prefix of f]
+    - min{g: f'g ~ f} |g|
+      - meaning: minimum number of lines that need to be added to f' to yield the same behavior as f
+    - min{ d1..dk st f'd1..dk ~ f} -log (
+        P(d1 | B, f', B'(f')) *
+        P(d2 | B, f'd1, B'(f'd1)) *
+        P(d3 | B, f'd1d2, B'(f'd1d2)) *
+        ...)
+    """
+    pass
+
+def gen_program(envs: list[dict], arg_exprs: list[Expr], n_lines: int, rand_arg_bounds: tuple[int, int],
+                line_types: list[type], line_type_weights: list[float] = None,
+                verbose=False):
+    """
+    Generates a program.
 
     Uses expressions containing randomness for arguments of each line object, but doesn't allow
     random colors.
     """
+    
     def is_valid(expr):
         for env in envs:
             try:
@@ -59,13 +89,13 @@ def gen_lines(envs: list[dict], arg_exprs: list[Expr], n_lines: int, rand_arg_bo
             except AssertionError:
                 return False
         return True
-
-    def choose_n_rand_args(n_args):
+    
+    def choose_n_rand_args(cap):
         lo, hi = rand_arg_bounds
-        lo = util.clamp(lo, 0, n_args)
-        hi = util.clamp(hi, 0, n_args)
+        lo = util.clamp(lo, 0, cap)
+        hi = util.clamp(hi, 0, cap)
         return random.randint(lo, hi)
-
+    
     assert rand_arg_bounds[0] <= rand_arg_bounds[1], 'Invalid rand argument bounds'
     rand_exprs, const_exprs = util.split(arg_exprs, lambda expr: expr.zs())
     lines = []
@@ -73,18 +103,17 @@ def gen_lines(envs: list[dict], arg_exprs: list[Expr], n_lines: int, rand_arg_bo
         n_tries = 0
         line_type = random.choices(population=line_types, weights=line_type_weights)[0]
         n_args = len(line_type.in_types) - 1
-        n_rand_args = choose_n_rand_args(n_args)
-        n_const_args = n_args - n_rand_args
-
+        
         # try choosing valid args for the line type
         # note: assumes that all in_types are integers (not true in general, but true of shapes)
         line = None
         while line is None:
             n_tries += 1
             if verbose and n_tries % 1000 == 0: print(f'[{len(lines)}, {line_type.__name__}]: {n_tries} tries')
-            
-            const_args = random.choices(population=const_exprs, k=n_const_args)
+            n_rand_args = choose_n_rand_args(n_args)
+            n_const_args = n_args - n_rand_args
             rand_args = random.choices(population=rand_exprs, k=n_rand_args)
+            const_args = random.choices(population=const_exprs, k=n_const_args)
             args = util.shuffled(const_args + rand_args)
             cand = line_type(*args, color=Num(random.randint(0, 9)))
             if is_valid(cand):
@@ -93,13 +122,12 @@ def gen_lines(envs: list[dict], arg_exprs: list[Expr], n_lines: int, rand_arg_bo
         lines = canonical_ordering(lines)
         lines = rm_dead_code(lines, envs)
         if verbose: print(f'[{len(lines)}, {line_type.__name__}]: {n_tries} tries')
-    return lines
+    return Seq(*lines)
 
 def rm_dead_code(lines, envs):
-
     def render(xs):
         return T.stack([Seq(*xs).eval(env) for env in envs])
-
+    
     orig_bmps = render(lines)
     out = []
     for i, line in enumerate(lines):
@@ -109,7 +137,6 @@ def rm_dead_code(lines, envs):
     return out
 
 def canonical_ordering(lines):
-
     def destructure(e):
         if isinstance(e, Point):
             return 0, e.x, e.y
@@ -117,40 +144,55 @@ def canonical_ordering(lines):
             return 1, e.x1, e.y1, e.x2, e.y2
         elif isinstance(e, Rect):
             return 2, e.x_min, e.y_min, e.x_max, e.y_max
-        
+    
     return sorted(lines, key=destructure)
 
 def extract_zs(envs):
     return [env['z'] for env in envs]
 
-def demo_gen_lines():
+def demo_gen_program():
     environments = seed_libs(5)
-    ls = gen_lines(envs=environments,
-                   arg_exprs=[Z(i) for i in range(LIB_SIZE)] + [Num(i) for i in range(0, 10)],
-                   n_lines=3,
-                   line_types=[Rect, Line, Point],
-                   line_type_weights=[4, 3, 1],
-                   rand_arg_bounds=(0, 2),
-                   verbose=True)
-    print(ls, extract_zs(environments))
-    p = Seq(*ls)
+    p = gen_program(envs=environments,
+                    arg_exprs=[Z(i) for i in range(LIB_SIZE)] + [Num(i) for i in range(0, 10)],
+                    n_lines=3,
+                    line_types=[Rect, Line, Point],
+                    line_type_weights=[4, 3, 1],
+                    rand_arg_bounds=(0, 2),
+                    verbose=True)
+    print(p, extract_zs(environments))
     ps = T.stack([
         p.eval(env) for env in environments
     ])
     viz.viz_mult(ps, text=p.simplify_indices())
 
-def demo_gen_data_worker():
-    fname = '../data/test1.dat'
-    gen_data_worker(fname=fname,
-                    n_envs=5, n_programs=10, n_lines=3,
-                    arg_exprs=[Z(i) for i in range(LIB_SIZE)] + [Num(i) for i in range(0, 10)],
+def demo_worker_gen_policy_data():
+    fname = '../data/policy-gen-test/'
+    arg_exprs = [Z(i) for i in range(LIB_SIZE)] + [Num(i) for i in range(0, 10)]
+    n_envs = 6
+    n_workers = 10
+    n_programs_per_worker = 10
+    workload_sz = n_programs_per_worker * n_workers
+    n_lines = 5
+    with mp.Pool(processes=n_workers) as pool:
+        pool.starmap(worker_gen_policy_data,
+                     [(f'{fname}{i}.dat', n_envs, n_programs_per_worker, n_lines,
+                       arg_exprs, (0, 2), [Rect, Line, Point], [4, 3, 1], True)
+                      for i in range(workload_sz)])
+
+    for i, (f, envs, bitmaps) in enumerate(util.load_glob_incremental(f'{fname}*.dat')):
+        print(i, f)
+
+def demo_gen_policy_data():
+    gen_policy_data(n_envs=5,
+                    n_programs=1000,
+                    n_lines_bounds=(1, 5),
                     rand_arg_bounds=(0, 2),
                     line_types=[Rect, Line, Point],
-                    line_type_weights=[4, 3, 1])
-    for f, envs in util.load_incremental(fname):
-        print(f, extract_zs(envs))
+                    line_type_weights=[4, 3, 1],
+                    fname_prefix='../data/policy-dat-test/')
 
 
 if __name__ == '__main__':
-    # demo_gen_lines()
-    demo_gen_data_worker()
+    # demo_gen_program()
+    # demo_worker_gen_policy_data()
+    demo_gen_policy_data()
