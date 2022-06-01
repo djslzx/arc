091ce7c3a03f711cc18a3c227e2b54384c82e690
David J. Lee <djslzx@gmail.com>
2022-06-01 17:22:20 -0400

    transform selected ARC tasks into bitmaps

--- src/arc_data.py
+++ src/arc_data.py
@@ -11,12 +11,31 @@ import numpy as np
 import torch as T
 import pdb
 
+import grammar
 import viz
-
+import util
 
 Mat = List[List]
 TaskPair = Dict[str, Mat]
 Task = Dict[str, List[TaskPair]]
+
+ARC_DIR = '../ARC/data/*'
+TASK_NAMES = [
+    "025d127b", "150deff5", "1caeab9d", "1e0a9b12", "1f876c06",
+    "1fad071e", "2281f1f4", "228f6490", "31aa019c", "39a8645d",
+    "3aa6fb7a", "3af2c5a8", "444801d8", "47c1f68c", "5c0a986e",
+    "6150a2bd", "62c24649", "6773b310", "6c434453", "6e82a1ae",
+    "7c008303", "7ddcd7ec", "97999447", "99b1bc43", "a3325580",
+    "a5313dff", "a78176bb", "a87f7484", "ac0a08a4", "b8cdaf2b",
+    "b94a9452", "c8f0f002", "d631b094", "d9fac9be", "ded97339",
+    "e179c5f4", "e5062a87", "e50d258f", "f76d97a5", "f9012d9b",
+]
+SELECTED_TASK_NAMES = [
+    '025d127b', '150deff5', '1caeab9d', '1e0a9b12', '39a8645d',
+    '3aa6fb7a', '3af2c5a8', '444801d8', '5c0a986e', '6150a2bd',
+    '6c434453', '6e82a1ae', '7c008303', '7ddcd7ec', '99b1bc43',
+    'a3325580',
+]
 
 def read_arc_file(filename: str) -> Tuple[str, Task]:
     with open(filename, 'r') as f:
@@ -81,31 +100,31 @@ def n_tasks_within_dim(height: int, width: int, dims: List[Dict]) -> int:
         n += dim['height'] <= height and dim['width'] <= width
     return n
 
-if __name__ == '__main__':
-    ARC_DIR = '../ARC/data/*'
-    TASK_NAMES = [
-        "025d127b", "150deff5", "1caeab9d", "1e0a9b12", "1f876c06",
-        "1fad071e", "2281f1f4", "228f6490", "31aa019c", "39a8645d",
-        "3aa6fb7a", "3af2c5a8", "444801d8", "47c1f68c", "5c0a986e",
-        "6150a2bd", "62c24649", "6773b310", "6c434453", "6e82a1ae",
-        "7c008303", "7ddcd7ec", "97999447", "99b1bc43", "a3325580",
-        "a5313dff", "a78176bb", "a87f7484", "ac0a08a4", "b8cdaf2b",
-        "b94a9452", "c8f0f002", "d631b094", "d9fac9be", "ded97339",
-        "e179c5f4", "e5062a87", "e50d258f", "f76d97a5", "f9012d9b",
-    ]
-    filenames = [name 
-                 for task_name in TASK_NAMES
+def plot_arc_dimensions():
+    filenames = [name
+                 for task_name in SELECTED_TASK_NAMES
                  for name in glob(f'{ARC_DIR}/{task_name}.json')]
+    tasks = [task for name, task in read_arc_files(filenames)]
+    dimensions = dims(tasks, ['input', 'output'])
+    print(plot_dims(dimensions))
+    n_dims = len(dimensions)
+    for n in range(18, 32):
+        n_filtered = n_tasks_within_dim(n, n, dimensions)
+        print(n, n_filtered)
 
-    tasks = read_arc_files(filenames)
-    for name, task in tasks:
+def selected_task_bitmaps() -> List[T.Tensor]:
+    filenames = [name
+                 for task_name in SELECTED_TASK_NAMES
+                 for name in glob(f'{ARC_DIR}/{task_name}.json')]
+    out_bitmaps = []
+    for name, task in read_arc_files(filenames):
         bitmaps = arc_task_as_tensors(task)
-        for b in bitmaps:
-            viz.viz(b, title=name)
+        padded_bitmaps = [util.pad_tensor(bitmap, h=grammar.B_H, w=grammar.B_W, padding_token=0)
+                          for bitmap in bitmaps]
+        out_bitmaps.extend(padded_bitmaps)
+    return out_bitmaps
 
-    # dimensions = dims(tasks, ['input', 'output'])
-    # print(plot_dims(dimensions))
-    # n_dims = len(dimensions)
-    # for n in range(18, 32):
-    #     n_filtered = n_tasks_within_dim(n, n, dimensions)
-    #     print(n, n_filtered)
+
+if __name__ == '__main__':
+    # plot_arc_dimensions()
+    print(selected_task_bitmaps())
